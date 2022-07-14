2cca2b7e45089eccb1f0e5ff8dffc6a6d315f76d
Neo TRAN <neo_tran@outlook.com>
2022-07-14 13:12:31 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.catalogosJSON = factory());
-}(this, (function () { 'use strict'; 
 function holaMundoPruebaDeCDN() {
     console.log('Hola mundo')
 }
-})));
